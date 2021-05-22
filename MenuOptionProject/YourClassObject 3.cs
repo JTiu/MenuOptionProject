149cac89f9ac45b00f3d7be1b2ce3cc2bdd2db31
James Tiu <jamestiulawyer@gmail.com>
2021-05-21 20:59:16 -0400

    changes to menu

--- MenuOptionProject/YourClassObject 3.cs
+++ MenuOptionProject/YourClassObject 3.cs
@@ -7,11 +7,7 @@ using System.Threading.Tasks;
 namespace MenuOptionNamespace
 {
     public class YourClassObject3
-    {//Variable HAS :  As a developer, I want to create a Contestant class that has a first name, last name, email address, and registration number
-
-        //public string Parameter1_string { get; set; }
-        //public string Parameter2_string { get; set; }
-        //public int Parameter3_int { get; set; }
+    {
 
         ////Method CAN DO
 
